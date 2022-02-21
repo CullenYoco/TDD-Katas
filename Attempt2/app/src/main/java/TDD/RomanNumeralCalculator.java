1277b86dbf4dfd2b3be6c2eef847825bab775aaa
Cullen Middleton <cullen.middleton@yoco.com>
2022-02-21 12:01:21 +0200

    Naive Start to RNC

--- Attempt2/app/src/main/java/TDD/RomanNumeralCalculator.java
+++ Attempt2/app/src/main/java/TDD/RomanNumeralCalculator.java
@@ -1,5 +1,27 @@
 package TDD;
 
 public class RomanNumeralCalculator {
+    public String intToRoman(int value) {
+        if (isInvalidValue(value)) {
+            throw new IllegalArgumentException();
+        }
 
+        if (value == 1) {
+            return "I";
+        }
+
+        if (value == 5) {
+            return "V";
+        }
+
+        if (value == 10) {
+            return "X";
+        }
+
+        return null;
+    }
+
+    private boolean isInvalidValue(int value) {
+        return value < 1 || value > 3999;
+    }
 }
--- Attempt2/app/src/test/java/TDD/RomanNumeralCalculatorTest.java
+++ Attempt2/app/src/test/java/TDD/RomanNumeralCalculatorTest.java
@@ -4,7 +4,43 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RomanNumeralCalculatorTest {
+    RomanNumeralCalculator rnc = new RomanNumeralCalculator();
 
-    
+    @Test
+    public void negativeTest() {
+        illegalArgumentTest(-1);
+        illegalArgumentTest(-10);
+        illegalArgumentTest(-1000);
+    }
 
+    @Test
+    public void zeroTest() {
+        illegalArgumentTest(0);
+    }
+
+    @Test
+    public void tooBigTest() {
+        illegalArgumentTest(4000);
+    }
+
+    @Test
+    public void oneTest() {
+        assertEquals("I", rnc.intToRoman(1));
+    }
+
+    @Test
+    public void fiveTest() {
+        assertEquals("V", rnc.intToRoman(5));
+    }
+
+    @Test
+    public void tenTest() {
+        assertEquals("X", rnc.intToRoman(10));
+    }
+
+    private void illegalArgumentTest(int value) {
+        assertThrows(IllegalArgumentException.class, () -> {
+            rnc.intToRoman(value);
+        });
+    }
 }
