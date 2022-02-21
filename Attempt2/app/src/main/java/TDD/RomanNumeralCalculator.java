8fedbce51dd47e1cb7d3572878002914c2a328a3
Cullen Middleton <cullen.middleton@yoco.com>
2022-02-21 12:29:38 +0200

    Base Roman Values with Map

--- Attempt2/app/src/main/java/TDD/RomanNumeralCalculator.java
+++ Attempt2/app/src/main/java/TDD/RomanNumeralCalculator.java
@@ -1,24 +1,34 @@
 package TDD;
 
+import java.util.HashMap;
+
 public class RomanNumeralCalculator {
+    HashMap<Integer, String> romanNumeralTranslator = new HashMap<Integer, String>();
+
+    public RomanNumeralCalculator() {
+        initTranslator();
+    }
+
+    private void initTranslator() {
+        romanNumeralTranslator.put(1, "I");
+        romanNumeralTranslator.put(5, "V");
+        romanNumeralTranslator.put(10, "X");
+        romanNumeralTranslator.put(50, "L");
+        romanNumeralTranslator.put(100, "C");
+        romanNumeralTranslator.put(500, "D");
+        romanNumeralTranslator.put(1000, "M");
+    }
+
     public String intToRoman(int value) {
         if (isInvalidValue(value)) {
             throw new IllegalArgumentException();
         }
 
-        if (value == 1) {
-            return "I";
-        }
+        return findRomanTranslation(value);
+    }
 
-        if (value == 5) {
-            return "V";
-        }
-
-        if (value == 10) {
-            return "X";
-        }
-
-        return null;
+    private String findRomanTranslation(int value) {
+        return romanNumeralTranslator.containsKey(value) ? romanNumeralTranslator.get(value) : null;
     }
 
     private boolean isInvalidValue(int value) {
--- Attempt2/app/src/test/java/TDD/RomanNumeralCalculatorTest.java
+++ Attempt2/app/src/test/java/TDD/RomanNumeralCalculatorTest.java
@@ -21,6 +21,8 @@ class RomanNumeralCalculatorTest {
     @Test
     public void tooBigTest() {
         illegalArgumentTest(4000);
+        illegalArgumentTest(5000);
+        illegalArgumentTest(10000);
     }
 
     @Test
@@ -36,6 +38,26 @@ class RomanNumeralCalculatorTest {
     @Test
     public void tenTest() {
         assertEquals("X", rnc.intToRoman(10));
+    }
+
+    @Test
+    public void fiftyTest() {
+        assertEquals("L", rnc.intToRoman(50));
+    }
+
+    @Test
+    public void oneHundredTest() {
+        assertEquals("C", rnc.intToRoman(100));
+    }
+
+    @Test
+    public void fiveHundredTest() {
+        assertEquals("D", rnc.intToRoman(500));
+    }
+
+    @Test
+    public void oneThousandTest() {
+        assertEquals("M", rnc.intToRoman(1000));
     }
 
     private void illegalArgumentTest(int value) {
